d818db12e4126872635241c70810efa7209b74b4
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-23 02:53:10 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string  Nome { get; set; }
+
+        // Associação de objectos: muito para 1
+        public ICollection<Seller> Seller { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+        public void AddSeller(Seller seller) 
+        {
+            Seller.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) 
+        {
+            return Seller.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
