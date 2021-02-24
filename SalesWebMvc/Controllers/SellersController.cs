f88fb1f0e3b87ffa6d54cdeb4e64becdac622973
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-24 03:23:42 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        public IActionResult Index()
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService) 
         {
-            return View();
+            _sellerService = sellerService;
+        }
+        public IActionResult Index() // Chamando o controlador - C
+        {
+            var list = _sellerService.FindAll(); // O controlador chama o Service do (Models) - M
+            return View(list); // E encaminha os dados para uma View - V
         }
     }
 }
